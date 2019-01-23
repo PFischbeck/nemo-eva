4094455554fe91716980cde3f46f42a4c44694e8
Philipp Fischbeck <philipp.fischbeck@hpi.de>
2019-01-23 15:09:31 +0100

    Use different data set for testing

--- src/helpers/classification.py
+++ src/helpers/classification.py
@@ -1,12 +1,15 @@
 import numpy
 
 from sklearn.model_selection import \
-    cross_val_score, GridSearchCV, StratifiedShuffleSplit
-
+    cross_val_score, train_test_split, GridSearchCV, StratifiedShuffleSplit
+from sklearn.metrics import accuracy_score
 
 class Classification:
     def __init__(self, X, Y, model, cv_grid=None):
         assert numpy.isfinite(X).all().all()
+
+
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=1/3, stratify=Y, shuffle=True, random_state=42)
 
         n_splits = 3
         cv = StratifiedShuffleSplit(n_splits=n_splits, test_size=1/n_splits, random_state=0)
@@ -14,13 +17,14 @@ class Classification:
         if cv_grid:
             grid_search = GridSearchCV(
                 model, cv_grid, "accuracy", cv=cv, refit=False)
-            grid_search.fit(X, Y)
+            grid_search.fit(X_train, Y_train)
             self.best_params = grid_search.best_params_
             model.set_params(**self.best_params)
         else:
             self.best_params = None
 
 
-        # Final test with different splits
-        cv2 = StratifiedShuffleSplit(n_splits=n_splits, test_size=1/n_splits, random_state=42)
-        self.accuracy = cross_val_score(model, X, Y, cv=cv2).mean()
+        # Final test with different data
+        model.fit(X_train, Y_train)
+        Y_true, Y_pred = Y_test, model.predict(X_test)
+        self.accuracy = accuracy_score(Y_true, Y_pred, normalize=True)
