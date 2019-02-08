d079520b9d3b7b39ddd990b670be56c679301b87
Philipp Fischbeck <philipp.fischbeck@hpi.de>
2019-02-08 14:58:28 +0100

    Use binary tree for connectivity of ER

--- src/helpers/generators.py
+++ src/helpers/generators.py
@@ -2,12 +2,39 @@ import random
 import bisect
 import itertools
 import math
+import numpy as np
 import networkit
 import collections
 #import pygirgs
 
 from helpers.graph_analysis import shrink_to_giant_component
 from helpers.powerlaw_estimation import powerlaw_fit
+
+def random_binary_tree(n):
+    t = networkit.Graph(n)
+    
+    vertices = list(range(n))
+    random.shuffle(vertices)
+
+    def gen_split(start, end):
+        if end-start==1:
+            return vertices[start]
+        if end-start==0:
+            return None
+        cur_root = vertices[start]
+        n_first = np.random.binomial(end-start-1, 1/2)
+        root_left = gen_split(start+1, start+1+n_first)
+        root_right = gen_split(start+1+n_first, end)
+        if root_left is not None:
+            t.addEdge(vertices[cur_root], vertices[root_left])
+        if root_right is not None:
+            t.addEdge(vertices[cur_root], vertices[root_right])
+        
+        return cur_root
+    
+    gen_split(0, n)
+
+    return t
 
 # Generate a random tree, return the graph
 def random_tree(n):
@@ -26,7 +53,7 @@ def random_tree(n):
 def random_weighted(choices, weights):
     if len(choices)==1:
         return choices[0]
-        
+
     cumdist = list(itertools.accumulate(weights))
     x = random.random() * cumdist[-1]
     return choices[bisect.bisect(cumdist, x)]
@@ -77,7 +104,7 @@ def generate_er(n, p, connected):
     graph = networkit.generators.ErdosRenyiGenerator(n, p).generate()
 
     if connected:
-        t = random_tree(n)
+        t = random_binary_tree(n)
         graph.merge(t)
 
     return graph
