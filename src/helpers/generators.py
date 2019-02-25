ef0e41c7f3414e450483c2ec8d1c7cf0e88de6b6
Philipp Fischbeck <philipp.fischbeck@hpi.de>
2019-02-25 17:40:02 +0100

    Fix incorrect number of components in CL

--- src/helpers/generators.py
+++ src/helpers/generators.py
@@ -332,7 +332,7 @@ def generate_chung_lu(degrees, connected):
         for _ in range(fit_iterations):
             # (Fairly) reduce degrees of high-degree vertices
             new_degrees = degrees.copy()
-            for i in range(components):
+            for i in range(components-1):
                 new_degrees[i] -= 1
             graph = networkit.generators.ChungLuGenerator(new_degrees).generate()
             comp = networkit.components.ConnectedComponents(graph)
@@ -340,7 +340,7 @@ def generate_chung_lu(degrees, connected):
             components = comp.numberOfComponents()
             
         new_degrees = degrees.copy()
-        for i in range(components):
+        for i in range(components-1):
             new_degrees[i] -= 1
         graph = networkit.generators.ChungLuGenerator(new_degrees).generate()
         make_connected_weighted(graph)
