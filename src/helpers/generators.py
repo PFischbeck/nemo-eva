19379f7787f3b750132557215b6b99be60a5ba14
Philipp Fischbeck <philipp.fischbeck@hpi.de>
2019-03-05 10:32:46 +0100

    Fix number of edges for CL

--- src/helpers/generators.py
+++ src/helpers/generators.py
@@ -331,16 +331,16 @@ def generate_chung_lu(degrees, connected):
         for _ in range(fit_iterations):
             # (Fairly) reduce degrees of high-degree vertices
             new_degrees = degrees.copy()
-            for i in range(components-1):
-                new_degrees[i] -= 1
+            for i in range((components-1)*2):
+                new_degrees[i%len(new_degrees)] -= 1
             graph = networkit.generators.ChungLuGenerator(new_degrees).generate()
             comp = networkit.components.ConnectedComponents(graph)
             comp.run()
             components = comp.numberOfComponents()
             
         new_degrees = degrees.copy()
-        for i in range(components-1):
-            new_degrees[i] -= 1
+        for i in range((components-1)*2):
+            new_degrees[i%len(new_degrees)] -= 1
         graph = networkit.generators.ChungLuGenerator(new_degrees).generate()
         make_connected_weighted(graph)
         return graph
