8ae06fdff5cda7b077da0f40ed861e4644bab4ee
Philipp Fischbeck <philipp.fischbeck@hpi.de>
2019-03-05 16:22:04 +0100

    Fix generated powerlaw distribution

--- src/helpers/powerlaw_estimation.py
+++ src/helpers/powerlaw_estimation.py
@@ -3,6 +3,7 @@ import powerlaw
 import helpers.tail_estimation as pl
 import networkit
 import sys
+import random
 
 #def powerlaw_fit(degrees):
 #    fit = powerlaw.Fit(degrees, fit_method='Likelihood', verbose=False)
@@ -29,8 +30,25 @@ def powerlaw_generate(n, max_deg, k, gamma):
     generator.run()
     generator.setMinimumFromAverageDegree(max(generator.getExpectedAverageDegree(), k))
     generator.run()
-    print("powerlaw: wanted k={}, but will get {}".format(k, generator.getExpectedAverageDegree()), file=sys.stderr)
+    #print("powerlaw: wanted k={}, but will get {}".format(k, generator.getExpectedAverageDegree()), file=sys.stderr)
 
-    degree_sequence = generator.getDegreeSequence(n)
+    degrees = generator.getDegreeSequence(n)
+    wanted_m2 = n * k
+    current_m2 = sum(degrees)
+
+    if current_m2 > wanted_m2:
+        diff = int(current_m2 - wanted_m2)
+        modifier = -1
+    else:
+        diff = int(wanted_m2 - current_m2)
+        modifier = +1
     
-    return degree_sequence
+    while diff:
+        pos = random.randrange(n)
+        if degrees[pos] + modifier >= 0:
+            degrees[pos] += modifier
+            diff -= 1
+
+    print("powerlaw: wanted k={}, will get {}".format(k, sum(degrees) / n, file=sys.stderr))
+
+    return degrees
